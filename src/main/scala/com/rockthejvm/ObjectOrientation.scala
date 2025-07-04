7df5a4d3310e56baefcee160d1fbc447d9402a14
Mahdi <mahdih2105@gmail.com>
2025-07-04 16:53:06 +0100

    Anonymous classes and instantiation. Added my own notes on anonymous classes with examples

--- src/main/scala/com/rockthejvm/ObjectOrientation.scala
+++ src/main/scala/com/rockthejvm/ObjectOrientation.scala
@@ -32,7 +32,7 @@ object ObjectOrientation extends App{
 
   // Abstract Class -  Cannot be instantiated on its own
   abstract class WalkingAnimal {
-    val hasLegs = true   // By default all fields and methods are public
+    val hasLegs = true   // By default, all fields and methods are public
     def walk(): Unit     // this can be restricted by 'private' or 'protected'
     // Private: Only the class has access to the member/method
     // Protected: Class and descendants have access
@@ -44,14 +44,50 @@ object ObjectOrientation extends App{
                                     // of objects that can be implemented in concrete Classes
   }
 
+  trait Philosopher {
+    def ?!(thought: String): Unit // ?! is a valid method name (kinda weird I guess)
+  }
+
   // Multi Trait Inheritance
   // single-class inheritance + multi trait - this is known as 'Mixing'
-  class Crocodile extends Animal with Carnivore {
+  class Crocodile extends Animal with Carnivore with Philosopher {
     override def eat(animal: Animal): Unit = println("Eating an animal...")
+    override def ?!(thought: String): Unit = println(s"I was thinking $thought")
   }
 
   val aCroc = new Crocodile
   aCroc.eat(aDog)   // Both these lines do the same thing
   aCroc eat aDog    // Infix Notation - ONLY available for methods with 1 argument
+  aCroc ?! "What if we could fly?"  // Looks like an operator but it's a method
+
+  // Operators - They are methods in Scala
+  val basicMath = 1 + 2
+  val anotherBasicMath = 1.+(2)   // These 2 are the same
+
+  // Anyonymous Classes
+  val dinosaur = new Carnivore {
+    override def eat(animal: Animal): Unit = println("I am a dinosaur so I can eat everything")
+  }
+  // This took some time to get my head around but here is what I learnt:
+  // Anonymous classes do not have a class name
+  // Anonymous classes are used for one time, quick behaviour etc.
+  // Anonymous classes do not have constructor args, unless the trait it uses does
+  // Anonymous classes define new behaviour without a class name
+  // HERE IS A COMPARISON between the two:
+  //
+  //   val newDog = new Animal {
+  //     override def eat(): Unit = println("big eater")
+  //   }
+  //   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  //   This would be an anonymous class because I defined
+  //   new behaviour and extended animal
+  //
+  //   And this code would be a normal class with instantiation:
+  //
+  //   class Dog extends Animal {
+  //     override def eat(): Unit = println("big eater")
+  //   }
+  //
+  //   val newDog = new Dog()
 
 }
