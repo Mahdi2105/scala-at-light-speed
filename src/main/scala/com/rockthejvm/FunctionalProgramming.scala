caa7bc7e31f262b21f7254310c1e66cc2d5e134c
Mahdi <mahdih2105@gmail.com>
2025-07-09 01:53:58 +0100

    Learnt some syntax sugars and noted differnces

--- src/main/scala/com/rockthejvm/FunctionalProgramming.scala
+++ src/main/scala/com/rockthejvm/FunctionalProgramming.scala
@@ -21,7 +21,7 @@ object FunctionalProgramming extends App{
   // ALL Scala functions are instances of these FUNCTION_X types
   // FunctionX: Function1, Function2... Function22 (22 max number of args allowed on func)
   val simpleIncrementor = new Function1[Int, Int] {  // Takes an Int, Returns an Int
-    override def apply (arg: Int): Int = arg + 1     // arg can be named anything, e.g num
+    override def apply (arg: Int): Int = arg + 1     // arg can be named anything, e.g. num
   }
 
   simpleIncrementor.apply(23)  // Returns 24
@@ -35,4 +35,16 @@ object FunctionalProgramming extends App{
 
   println(stringConcat("My name is:", "Mahdi"))
 
+  // Syntax Sugars - alternative syntax to replace heavier boilerplate
+  // val doubler: Function1[Int, Int] = (x: Int) => 2 * x
+  // This is the same as:
+  //    val doubler = new Function1[Int, Int] {
+  //      override def apply(x: Int): Int = 2 * x
+  //    }
+
+  // This can be further simplified to:
+  val doubler: Int => Int = (x: Int) => 2 * x
+
+  // And this further to:
+  // val doubler = (x: Int) => 2* x
 }
